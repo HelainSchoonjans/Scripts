ea3f77fa43936184915aa3baaa48bee4f1e38e85
Hélain Schoonjans <helain.schoonjans@eurekanetwork.org>
2019-11-15 15:39:40 +0100

    Method to create random alphanumerics

--- python3/randomString.py
+++ python3/randomString.py
@@ -1,9 +1,17 @@
 import random
 import string
 
-def randomString(stringLength=10):
+
+def randomString(stringLength=60):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
+    return ''.join(random.choice(letters) for _ in range(stringLength))
 
-print(randomString(32).upper())
+
+def randomAlphanumeric(stringLength=60):
+    return ''.join(
+        random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(stringLength))
+
+
+print(randomString(60).upper())
+print(randomAlphanumeric(60).upper())
